99a86488de17a3621240cd900d4e5aa3006fd2f4
lucas nogueira <lucasnogueira@gmail.com>
2014-04-16 14:32:01 -0300

    fallback para usuario nao autenticado

--- lib/api/live_rail.rb
+++ lib/api/live_rail.rb
@@ -1461,9 +1461,7 @@ class LiveRail
       @cache.set(path, body, response) if @enable_cache && !@chache.nil?
     end
 
-    p response
-
-    if response['liverailapi']['status'] == "fail" && response['liverailapi']['error']['code'] == '6'
+    if response['liverailapi']['status'] == "fail" && response['liverailapi']['error'][0]['code'] == '6'
       response = self.login
       if response['liverailapi']['status'] == "success" && path != "/login/"
         response = self.request path, body
