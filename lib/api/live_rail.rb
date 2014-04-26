c55cbffeeee28b8382d1154a681c0e46cedd2ce2
lucas nogueira <lucasnogueira@gmail.com>
2014-04-26 15:33:50 -0300

    add error 5

--- lib/api/live_rail.rb
+++ lib/api/live_rail.rb
@@ -1461,7 +1461,9 @@ class LiveRail
       @cache.set(path, body, response) if @enable_cache && !@chache.nil?
     end
 
-    if response['liverailapi']['status'] == "fail" && (response['liverailapi']['error'][0]['code'] == '6' or response['liverailapi']['error'][0]['code'] == '2' or response['liverailapi']['error'][0]['code'] == '5')
+
+
+    if response['liverailapi']['status'] == "fail" && (['6', '2', '5'].include? response['liverailapi']['error'][0] ? response['liverailapi']['error'][0]['code'] : response['liverailapi']['error']['code'])
       response = login
       if response['liverailapi']['status'] == "success" && path != "/login/"
         response = request path, body
